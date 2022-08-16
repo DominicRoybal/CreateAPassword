4444af3ec6aa50d9560db08aacd61ad18000f466
DominicRoybal <109049233+DominicRoybal@users.noreply.github.com>
2022-08-16 09:32:34 -0700

    Making sure my git add works

---
+++ script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// This first git push it to make sure I have completed the gitbash to link this to my github acount.
