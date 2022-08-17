03473f8d3e960121a5de0f3a4781d333a98c7994
DominicRoybal <109049233+DominicRoybal@users.noreply.github.com>
2022-08-17 11:25:53 -0700

    I added a function allowing password credencial wuestions to pop up for the user including uppercase and lowercase.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+//This is a variable labeling the reg generate password button on screen
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,9 +12,8 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", lowercaseprompt);
 
-// This first git push it to make sure I have completed the gitbash to link this to my github acount.
 
 // I decided to start with variables.
 var Length = [1234567891234567];
@@ -22,5 +22,36 @@ var uppercase = [QWERTYUIOPLKJHGFDSAZXCVBNM];
 var numbers = [0123456789];
 var specialcharacters = ["!","@","#","$","%","^","&","*","(",")",];
 
+// Created a function allowing the passowor crediential questions to run.
+// The first is prompting the user to choose options of using or not useing lowercase letters
+function lowercaseprompt() {
+  var userlowercase = window.confirm ("Would you like you password to include lowecase letters?");
+  if (userlowercase){
+  alert ("You have decided to use lowercase letters in your password");
+} else if (!userlowercase) {
+  alert ("You have decided not to use lowercase letters in your password");
+}
+// This second option is propting the user about uppercase letters
+ var useruppercase = window.confirm ("Would you like you password to include uppercase letters?");
+if (useruppercase) {
+  alert ("You have decided to use uppercase letters in your password");
+} else if (!useruppercase) {
+  alert ("You have decided not to use uppercase letters in your password");
+}
 
 
+
+}
+
+
+// Everything below is extra
+
+
+generateBtn.addEventListener("click", lengthprompt);
+
+function lengthprompt() {
+  var userlength = Number(window.prompt ("Please choose how many characters you would like your password to be. Please choose a number between 8 to 16"));
+  alert ("You have choosen a password including " + userlength + " characters.");
+}
+
+//pg72 slice for password length
