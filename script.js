4c3cb09582128fe1ae8766bc45f36f9a4205f6a6
DominicRoybal <109049233+DominicRoybal@users.noreply.github.com>
2022-08-17 15:00:34 -0700

    I added the code to add the users choices to the password credentials

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ var Length = [1234567891234567];
 var lowercase = [qwertyuioplkjhgfdsazxcvbnm];
 var uppercase = [QWERTYUIOPLKJHGFDSAZXCVBNM];
 var numbers = [0123456789];
-var specialcharacters = ["!","@","#","$","%","^","&","*","(",")",];
+var specialcharacters = ["!","@","#","$","%","^","&","*","(",")"];
+
 
 // Created a function allowing the passowor crediential questions to run.
 // The first is prompting the user to choose options of using or not useing lowercase letters
@@ -52,19 +53,59 @@ if (useruppercase) {
  } else if (!userspecial) {
   alert ("You have decided not to include special characters in your password");
  }
+// This next section deals with the amount of characters that appear in the password
+ var userlength = Number(window.prompt ("Please choose how many characters you would like your password to be. Please choose a number between 8 to 16"));
+  if (userlength < 8, userlength > 16) {
+    alert ("The amount of characters in your password must be between 8 and 16 characters");
+  } else if (7 < userlength < 17){
+    alert ("You have choosen a password containing " + userlength + " characters.");
+  }
+  alert ("Press ok to generate your password!");
+
+//This next section will be used to fill in the password credentials
+ var PASS = [1234567]
+  if (userlength = 8) {
+    PASS.push (8);
+  } else if (userlength = 9){
+    PASS.push (9);
+  } else if (userlength = 10) {
+    PASS.push (10);
+  } else if (userlength = 11) {
+    PASS.push (11);
+  } else if (userlength = 12) {
+    PASS.push (12);
+  } else if (userlength = 13) {
+    PASS.push (13);
+  } else if (userlength = 14) {
+    PASS.push (14);
+  } else if (userlength = 15) {
+    PASS.push (15);
+  } else if (userlength = 16) {
+    PASS.push (16);
+  } else if (8 > userlength > 16) {
+    PASS = ["Between 8 and 16 characters please"]
+  }
+  //Creating rulles for the code based on the users answer
+  if (userlowercase) {
+    PASS[index] = lowercase[index];
+  } else if (!userlowercase) {
+    PASS[index] != lowercase[index];
+  }
+  if (useruppercase) {
+    PASS[index] = uppercase[index];
+  } else if (!uppercase) {
+    PASS[index] != uppercase[index];
+  }
+  if (usernumber) {
+    PASS[index] = numbers[index];
+  } else if (!usernumber) {
+    PASS[index] != numbers[index];
+  }
+  if (userspecial) {
+    PASS[index] = specialcharacters[index];
+  } else if (!userspecial) {
+    PASS[index] != specialcharacters[index];
+  }
 
 
 }
-
-
-// Everything below is extra
-
-
-generateBtn.addEventListener("click", lengthprompt);
-
-function lengthprompt() {
-  var userlength = Number(window.prompt ("Please choose how many characters you would like your password to be. Please choose a number between 8 to 16"));
-  alert ("You have choosen a password including " + userlength + " characters.");
-}
-
-//pg72 slice for password length
